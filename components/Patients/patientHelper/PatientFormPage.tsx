788d8b8c32df60f17414c778f86a5f982486be3d
joelralphdeveloper <163519284+joelralphdeveloper@users.noreply.github.com>
2024-08-06 10:33:00 +0800

    chore: added vertical tabs

--- components/Patients/patientHelper/PatientFormPage.tsx
+++ components/Patients/patientHelper/PatientFormPage.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 
 import UseRouting from "@/components/helperFunctions/UseRouting";
+import { VerticalTabs } from "@/components/vertical-tabs/vertical-tabs";
 
 import PatientFormHelper from "./PatientFormHelper";
 
@@ -41,56 +42,57 @@ const PatientFormPage: React.FC<PatientFormPageProps> = ({
   };
 
   return (
-    <div className="flex h-screen w-full">
-      {/* Sidebar Navigation */}
-      <nav className="h-full w-[15%] shrink-0 overflow-y-auto border-r border-gray-200 bg-gray-50">
-        <div className="p-4">
-          <button
-            onClick={(event) => {
-              handlePatientDetails(mode === "view" ? "edit" : "view", "mode");
-              event.stopPropagation();
-            }}
-            className="w-full rounded-md bg-gray-200 px-4 py-2 text-gray-700 hover:bg-gray-300"
-          >
-            {mode === "view" ? "Add Medical Details" : "View Medical Details"}
-          </button>
-        </div>
-        <ul className="p-4">
-          {Object.values(PATIENT_DETAILS).map((detail) => (
-            <li key={detail.VALUE} className="mb-2">
-              <button
-                onClick={() => handleTabChange(detail.VALUE)}
-                className={`w-full px-4 py-2 text-left transition-colors duration-300 ease-in-out ${
-                  currentTab.tab === detail.VALUE
-                    ? "bg-blue-700 text-white"
-                    : "text-gray-600 hover:bg-blue-100"
-                }`}
-              >
-                {detail.LABEL}
-              </button>
-            </li>
-          ))}
-        </ul>
-        <div className="p-4">
-          <button
-            onClick={(event) => {
-              routePath(`/admin/patients`);
-              event.stopPropagation();
-            }}
-            className="w-full rounded-md bg-rose-500 px-4 py-2 text-white hover:bg-rose-400"
-          >
-            Back
-          </button>
-        </div>
-      </nav>
+    <VerticalTabs />
+    // <div className="flex h-screen w-full">
+    //   {/* Sidebar Navigation */}
+    //   <nav className="h-full w-[15%] shrink-0 overflow-y-auto border-r border-gray-200 bg-gray-50">
+    //     <div className="p-4">
+    //       <button
+    //         onClick={(event) => {
+    //           handlePatientDetails(mode === "view" ? "edit" : "view", "mode");
+    //           event.stopPropagation();
+    //         }}
+    //         className="w-full rounded-md bg-gray-200 px-4 py-2 text-gray-700 hover:bg-gray-300"
+    //       >
+    //         {mode === "view" ? "Add Medical Details" : "View Medical Details"}
+    //       </button>
+    //     </div>
+    //     <ul className="p-4">
+    //       {Object.values(PATIENT_DETAILS).map((detail) => (
+    //         <li key={detail.VALUE} className="mb-2">
+    //           <button
+    //             onClick={() => handleTabChange(detail.VALUE)}
+    //             className={`w-full px-4 py-2 text-left transition-colors duration-300 ease-in-out ${
+    //               currentTab.tab === detail.VALUE
+    //                 ? "bg-blue-700 text-white"
+    //                 : "text-gray-600 hover:bg-blue-100"
+    //             }`}
+    //           >
+    //             {detail.LABEL}
+    //           </button>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //     <div className="p-4">
+    //       <button
+    //         onClick={(event) => {
+    //           routePath(`/admin/patients`);
+    //           event.stopPropagation();
+    //         }}
+    //         className="w-full rounded-md bg-rose-500 px-4 py-2 text-white hover:bg-rose-400"
+    //       >
+    //         Back
+    //       </button>
+    //     </div>
+    //   </nav>
 
-      {/* Component renderer */}
-      <PatientFormHelper
-        mode={mode}
-        currentTab={currentTab}
-        PATIENT_DETAILS={PATIENT_DETAILS}
-      />
-    </div>
+    //   {/* Component renderer */}
+    //   <PatientFormHelper
+    //     mode={mode}
+    //     currentTab={currentTab}
+    //     PATIENT_DETAILS={PATIENT_DETAILS}
+    //   />
+    // </div>
   );
 };
 
